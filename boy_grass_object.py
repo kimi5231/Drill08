0342ef6f32120d3c414146d52878d2232253eeb4
kimi5231 <djwjod@naver.com>
2023-10-05 12:58:03 +0900

    class practice

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,31 @@
 from pico2d import *
+import random
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -11,11 +36,46 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+
+    running = True
+    world = []
+
+    grass = Grass()  # 클래스를 이용해 객체를 찍어냄.
+    world.append(grass)
+
+    team = [Boy() for i in range(11)]
+    world += team
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()  # 객체들의 상호작용 결과 업데이트.
+    render_world()
+    delay(0.05)
 
 # finalization code
 
